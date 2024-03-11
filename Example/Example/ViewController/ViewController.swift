1a1d27e7782d425e16e5e705527b6dab190ee4b9
William.Weng <William.Weng@iit.com.hk>
2024-03-11 14:35:35 +0800

    - add example

---
+++ Example/Example/ViewController/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by William.Weng on 2024/3/11.
+//
+
+import UIKit
+import WWCaptchaView
+
+// MARK: - ViewController
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var captchaLabel: UILabel!
+    @IBOutlet weak var captchaView: WWCaptchaView!
+
+    let stringModel: WWCaptchaView.RandomStringModel = .init(
+        digits: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890甲乙丙丁戊己庚辛壬癸",
+        length: 4,
+        font: UIFont.systemFont(ofSize: 56),
+        upperBound: 10
+    )
+    
+    let lineModel: WWCaptchaView.RandomLineModel = .init(
+        count: 5,
+        width: 1.0
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captchaView.configure(delegate: self, stringModel: stringModel, lineModel: lineModel)
+    }
+}
+
+// MARK: - WWCaptchaViewDelegate
+extension ViewController: WWCaptchaViewDelegate {
+    
+    func captcha(view: WWCaptchaView, string: String?) {
+        captchaLabel.text = string
+    }
+}
